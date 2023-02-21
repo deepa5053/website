09286799a7a4c147cce78bfbc0c5576114d652eb
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2023-02-21 21:15:41 +0100

    sign up form set

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -10,11 +10,11 @@ const SignIn = () => {
     <div className="form">
           <div className="form-body">
               <div className="username">
-                  <label className="form__label" for="firstName">First Name </label>
+                  <label className="form__label" for="firstName">First Name :- </label>
                   <input className="form__input" type="text" id="firstName" placeholder="First Name"/>
               </div>
               <div className="password">
-                  <label className="form__label" for="password">Password </label>
+                  <label className="form__label" for="password">Password :- </label>
                   <input className="form__input" type="password"  id="password" placeholder="Password"/>
               </div>
           </div>
